ab30e2d0c5d848c950107c610afa7311a5fed687
iwsl <iwsl@iwsl>
2024-01-25 22:04:52 +0800

    update game core logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+// import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyAPP from './MyAPP';
+import './MyAPP.css';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MyAPP />
   </React.StrictMode>
 );
 
